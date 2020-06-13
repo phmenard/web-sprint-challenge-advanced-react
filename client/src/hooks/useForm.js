354675c5563d8250ae8f9f1ac044408af2db923c
unknown <clmonitor2013@gmail.com>
2020-06-12 21:42:35 -0400

    doing my best

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,32 @@
+import { useState } from "react";
+
+
 // write your custom hook here to control your checkout form
+export const useForm = (key, initialValues, cb) => {
+    // handle inputs
+    const [setValues, values] = useState(key, initialValues);
+  
+    const handleChanges = e => {
+      console.log(e.target.name);
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    // handleSubmit
+    const handleSubmit = e => {
+      if (e) e.preventDefault();
+      cb();
+    };
+
+    // clear the form
+    const clearForm = e => {
+      e.preventDefault();
+      setValues(initialValues);
+    };
+
+
+    // return the hook
+    return [values, clearForm, handleSubmit, handleChanges];
+  };
